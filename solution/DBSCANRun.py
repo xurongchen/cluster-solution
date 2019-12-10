e589dbef6cfc0f9eb1a46560eeb3cb8e034462e7
xurongchen <1459572152@qq.com>
2019-12-10 19:59:59 +0800

    No pca for DBSCAN

--- solution/DBSCANRun.py
+++ solution/DBSCANRun.py
@@ -1,7 +1,7 @@
 from Data import Data
 test = Data()
 test.ReadData('data.csv')
-test = test.pca(0.95)
+# test = test.pca(0.95)
 result = test.DBSCAN(eps=3.7417,min_samples=160)
 result.ShowLabelInfo()
 result.Draw()
