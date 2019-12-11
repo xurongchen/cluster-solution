6ad2c2be5b2b98fb03f552e380393be402015c18
给我七平米 <30563273+sadyan9123@users.noreply.github.com>
2019-12-11 19:06:04 +0800

    Update KMeans_SelectK.py

--- solution/KMeans_SelectK.py
+++ solution/KMeans_SelectK.py
@@ -1,19 +1,22 @@
 from Data import Data
 import matplotlib.pyplot
-
+import time
 
 SSE = [] # sum of the squared errors
 TestUpperBound = 15
 for k in range(1,TestUpperBound):
     # print('Now @ k = {0}'.format(k))
+    start=time.time()
     testK = Data()
     testK.ReadData('data.csv')
     result = testK.KMeans(k)
     result.ShowLabelInfo(output=False)
+    time1=time.time()-start
     silScore = result.getScore(method='Silhouette')
     calScore = result.getScore(method='CalinskiHarabasz')
     davScore = result.getScore(method='DaviesBouldin')
-    print('K:',k,'SIL:{:.4f}'.format(silScore),'CAL:{:.4f}'.format(calScore),'DAV:{:.4f}'.format(davScore),'CNT:',sum(result.distributionInfo['Num']),'LC',result.distributionInfo['Num'])
+    
+    print('TIME:{:.2f}'.format(time1),'K:',k,'SIL:{:.4f}'.format(silScore),'CAL:{:.4f}'.format(calScore),'DAV:{:.4f}'.format(davScore),'CNT:',sum(result.distributionInfo['Num']),'LC',result.distributionInfo['Num'])
     SSE.append(testK.midResult.inertia_)
 
 x = range(1,TestUpperBound)
